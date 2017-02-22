54e2061e8e0bc5c8ce052283de055cd92cf097de
max1 <maximbobyleu@gmail.com>
2017-02-22 04:06:41 +0300

    REST: Jackson jsr310 support, minor fixes.

--- src/main/java/com/assignment/dao/ReservationRepository.java
+++ src/main/java/com/assignment/dao/ReservationRepository.java
@@ -2,14 +2,17 @@ package com.assignment.dao;
 
 import com.assignment.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    List<Reservation> findByRoomId(Long roomId);
+    List<Reservation> findByRoomId(@Param("roomId") Long roomId);
 
-    List<Reservation> findByRoomIdAndStartDateBetween(Long roomId, LocalDateTime startDate, LocalDateTime endDate);
+    List<Reservation> findByRoomIdAndStartDateBetween(@Param("roomId") Long roomId,
+                                                      @Param("startDate") LocalDateTime startDate,
+                                                      @Param("endDate") LocalDateTime endDate);
 
 }
