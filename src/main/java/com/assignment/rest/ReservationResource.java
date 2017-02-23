0b595a8e9b0d21eaf778477f60ae42d76a5270f5
max1 <maximbobyleu@gmail.com>
2017-02-23 12:01:41 +0300

    REST: Apply code formatter for ReservationResource

--- src/main/java/com/assignment/rest/ReservationResource.java
+++ src/main/java/com/assignment/rest/ReservationResource.java
@@ -1,8 +1,12 @@
 package com.assignment.rest;
 
-import com.assignment.domain.Reservation;
-import com.assignment.exception.BookingException;
-import com.assignment.service.ReservationService;
+import static com.assignment.rest.ErrorResponse.anErrorMessage;
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +15,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.validation.Valid;
-
-import static com.assignment.rest.ErrorResponse.anErrorMessage;
-import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import com.assignment.domain.Reservation;
+import com.assignment.exception.BookingException;
+import com.assignment.service.ReservationService;
 
 @RepositoryRestController
 @Validated
+@RequestMapping(value = "/reservations")
 public class ReservationResource {
 
     private final ReservationService reservationService;
@@ -29,13 +31,16 @@ public class ReservationResource {
         this.reservationService = reservationService;
     }
 
-    @RequestMapping(method = POST, value = "/reservations")
+    @RequestMapping(method = POST)
     public @ResponseBody ResponseEntity<?> createReservation(@RequestBody @Valid final Reservation bookingRequest) {
         try {
             Reservation reservation = reservationService.save(bookingRequest);
-            return ResponseEntity.status(CREATED).body(reservation);
+            return ResponseEntity.status(CREATED)
+                    .body(reservation);
         } catch (BookingException ex) {
-            return ResponseEntity.status(CONFLICT).body(anErrorMessage(ex.getMessage()));
+            return ResponseEntity.status(CONFLICT)
+                    .body(anErrorMessage(ex.getMessage()));
         }
     }
+
 }
